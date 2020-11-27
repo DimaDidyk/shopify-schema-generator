27dfb231c89b6d6a587859c874f07735d7f4b083
DimaDidyk <didykdima11@gmail.com>
2020-11-27 20:47:12 +0200

    Updates

---
+++ precache-manifest.b0b8156146d6c63ae1c93c50f1502d5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0bd55d784c3d32cd279ee22d127512",
+    "url": "/shopify-schema-generator/index.html"
+  },
+  {
+    "revision": "a209bea4ffa9005abf8f",
+    "url": "/shopify-schema-generator/static/css/main.c6513e3a.chunk.css"
+  },
+  {
+    "revision": "bec2e24716e9f0617932",
+    "url": "/shopify-schema-generator/static/js/2.e5418591.chunk.js"
+  },
+  {
+    "revision": "25ee36cecf3742d8e72fa8504c054097",
+    "url": "/shopify-schema-generator/static/js/2.e5418591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a209bea4ffa9005abf8f",
+    "url": "/shopify-schema-generator/static/js/main.d6dd2ecc.chunk.js"
+  },
+  {
+    "revision": "5b80738e22a2cc841447",
+    "url": "/shopify-schema-generator/static/js/runtime-main.96d7065b.js"
+  }
+]);
